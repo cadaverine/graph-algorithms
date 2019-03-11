61c120a6f38ca50123e001e10d261208ccf6c9d1
cadaverine <kennville@gmail.com>
2019-03-11 23:30:36 +0300

    feat(Graph): update Graph realization

--- graph/graph.go
+++ graph/graph.go
@@ -1,5 +1,7 @@
 package graph
 
+import . "../list"
+
 type Vertex struct {
 	data interface{}
 }
@@ -8,18 +10,32 @@ type Edge struct {
 	weight int
 	from   *Vertex
 	to     *Vertex
-	next   *Edge
-}
-
-func (edge *Edge) Next() interface{} {
-	return edge.next
-}
-
-type EdgeList struct {
-	head *Edge
 }
 
 // Graph - реализация графа в виде списка смежности
 type Graph struct {
-	edgeList []EdgeList
+	adjacencyMap map[*Vertex]*List
+}
+
+// InitializeGraph - создание графа (аналог конструктора
+func InitializeGraph() *Graph {
+	return &Graph{make(map[*Vertex]*List, 0)}
+}
+
+// AddVertex - создать вершину из данных и добавить в граф
+func (graph *Graph) AddVertex(data interface{}) {
+	vertex := Vertex{data}
+	graph.adjacencyMap[&vertex] = &List{}
+}
+
+// AddEdge - создать ребро и добавить в граф
+func (graph *Graph) AddEdge(from, to *Vertex, weight int) {
+	edge := Edge{weight, from, to}
+
+	if _, keyExist := graph.adjacencyMap[from]; keyExist {
+		graph.adjacencyMap[from].AddData(&edge)
+	} else {
+		graph.adjacencyMap[from] = &List{}
+		graph.adjacencyMap[from].AddData(&edge)
+	}
 }
