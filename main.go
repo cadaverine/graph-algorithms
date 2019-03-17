f6c9bbe3549e4a83f99262ded1548418aaac6f9b
cadaverine <kennville@gmail.com>
2019-03-17 23:47:53 +0300

    feat(main): add simple Graph creation

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"./graph"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	gr := graph.InitializeGraph()
+
+	gr.AddVertex(1)
+	gr.AddVertex(2)
+	gr.AddVertex(3)
+	gr.AddVertex(4)
+	gr.AddVertex(5)
+	gr.AddVertex(6)
+	gr.AddVertex(7)
+
+	gr.AddEdgeData(1, 3, 5)
+	gr.AddEdgeData(1, 4, 2)
+	gr.AddEdgeData(5, 7, 1)
+	gr.AddEdgeData(3, 1, 3)
+
+	fmt.Println(gr)
 }
