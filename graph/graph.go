26f416ea39090f882ac044aeb51c98213e5b0814
cadaverine <kennville@gmail.com>
2019-03-18 14:10:06 +0300

    fix(Graph): fix import, replace edge, vertex implementation

--- graph/graph.go
+++ graph/graph.go
@@ -3,43 +3,23 @@ package graph
 import (
 	"fmt"
 
-	. "../list"
+	"../list"
 )
-
-// Vertex - вершина графа
-type Vertex struct {
-	data interface{}
-}
-
-func (vertex *Vertex) String() string {
-	return fmt.Sprint(vertex.data)
-}
-
-// Edge - ребро графа
-type Edge struct {
-	weight int
-	from   *Vertex
-	to     *Vertex
-}
-
-func (edge *Edge) String() string {
-	return fmt.Sprint(edge.from) + " -> " + fmt.Sprint(edge.to)
-}
 
 // Graph - реализация графа в виде списка смежности
 type Graph struct {
-	adjacencyMap map[*Vertex]*List
+	adjacencyMap map[*Vertex]*list.List
 }
 
-// InitializeGraph - создание графа (аналог конструктора
+// InitializeGraph - создание графа (аналог конструктора)
 func InitializeGraph() *Graph {
-	return &Graph{make(map[*Vertex]*List, 0)}
+	return &Graph{make(map[*Vertex]*list.List, 0)}
 }
 
 // AddVertex - создать вершину из данных и добавить в граф
 func (graph *Graph) AddVertex(data interface{}) {
 	vertex := Vertex{data}
-	graph.adjacencyMap[&vertex] = &List{}
+	graph.adjacencyMap[&vertex] = &list.List{}
 }
 
 // AddEdge - создать ребро и добавить в граф
@@ -49,7 +29,7 @@ func (graph *Graph) AddEdge(from, to *Vertex, weight int) {
 	if _, keyExist := graph.adjacencyMap[from]; keyExist {
 		graph.adjacencyMap[from].AddData(&edge)
 	} else {
-		graph.adjacencyMap[from] = &List{}
+		graph.adjacencyMap[from] = &list.List{}
 		graph.adjacencyMap[from].AddData(&edge)
 	}
 }
