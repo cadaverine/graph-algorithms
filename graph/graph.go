b5e8c8426ab6d068393265bbe33f26735404fab4
cadaverine <kennville@gmail.com>
2019-03-20 13:28:31 +0300

    feat(Graph): add 'GetVertexByID', 'AddEdgeByIDs' methods; update 'AddEdgeByData', 'AddVertex' methods

--- graph/graph.go
+++ graph/graph.go
@@ -10,48 +10,63 @@ import (
 // Graph - реализация графа в виде списка смежности
 type Graph struct {
 	adjacencyMap map[*Vertex]*list.List
+	verteces     []*Vertex
 }
 
 // InitializeGraph - создание графа (аналог конструктора)
 func InitializeGraph() *Graph {
-	return &Graph{make(map[*Vertex]*list.List, 0)}
+	return &Graph{
+		adjacencyMap: make(map[*Vertex]*list.List, 0),
+		verteces:     make([]*Vertex, 0),
+	}
 }
 
 // AddVertex - создать вершину из данных и добавить в граф
-func (graph *Graph) AddVertex(data interface{}) {
-	vertex := Vertex{data}
-	graph.adjacencyMap[&vertex] = &list.List{}
+func (graph *Graph) AddVertex(data interface{}) *Vertex {
+	id := len(graph.verteces)
+
+	vertex := &Vertex{data, id}
+
+	graph.verteces = append(graph.verteces, vertex)
+	graph.adjacencyMap[vertex] = &list.List{}
+
+	return vertex
+}
+
+// GetVertexByID - получить вершину графа по его ID
+func (graph *Graph) GetVertexByID(id int) *Vertex {
+	return graph.verteces[id]
 }
 
 // AddEdge - создать ребро и добавить в граф
 func (graph *Graph) AddEdge(from, to *Vertex, weight int) {
-	edge := Edge{weight, from, to}
+	edge := &Edge{weight, from, to}
 
 	if _, keyExist := graph.adjacencyMap[from]; keyExist {
-		graph.adjacencyMap[from].AddData(&edge)
+		graph.adjacencyMap[from].AddData(edge)
 	} else {
 		graph.adjacencyMap[from] = &list.List{}
-		graph.adjacencyMap[from].AddData(&edge)
+		graph.adjacencyMap[from].AddData(edge)
 	}
 }
 
-// AddEdgeData - создать ребро из данных и добавить в граф
-func (graph *Graph) AddEdgeData(fromData, toData interface{}, weight int) {
-	for from := range graph.adjacencyMap {
-		if from.data == fromData {
-			to := Vertex{toData}
+// AddEdgeByData - создать ребро из данных и добавить в граф
+func (graph *Graph) AddEdgeByData(fromData, toData interface{}, weight int) {
+	to := graph.AddVertex(toData)
+	from := graph.AddVertex(fromData)
 
-			graph.AddEdge(from, &to, weight)
-			return
-		}
-	}
-
-	to := Vertex{toData}
-	from := Vertex{fromData}
-
-	graph.AddEdge(&from, &to, weight)
+	graph.AddEdge(from, to, weight)
 }
 
+// AddEdgeByIDs - создать ребро из между существующими вершинами
+func (graph *Graph) AddEdgeByIDs(fromID int, toID int, weight int) {
+	to := graph.GetVertexByID(toID)
+	from := graph.GetVertexByID(fromID)
+
+	graph.AddEdge(from, to, weight)
+}
+
+// String - реализация интерфейса Stringer
 func (graph *Graph) String() (out string) {
 	for key, value := range graph.adjacencyMap {
 		out += fmt.Sprint(key) + ": " + fmt.Sprint(value) + "\n"
@@ -59,12 +74,6 @@ func (graph *Graph) String() (out string) {
 
 	return out
 }
-
-// DFS (depth-first search) - поиск в глубину
-// func (graph *Graph) DFS(data interface{}, from *Vertex) *Vertex {
-// 	list := graph.adjacencyMap[from]
-// 	stack := stack.Stack{}
-// }
 
 // BFS (breadth-first search) - поиск в ширину
 func (graph *Graph) BFS(data interface{}, from *Vertex) *Vertex {
@@ -95,3 +104,9 @@ func (graph *Graph) BFS(data interface{}, from *Vertex) *Vertex {
 
 	return nil
 }
+
+// DFS (depth-first search) - поиск в глубину
+// func (graph *Graph) DFS(data interface{}, from *Vertex) *Vertex {
+// 	list := graph.adjacencyMap[from]
+// 	stack := stack.Stack{}
+// }
