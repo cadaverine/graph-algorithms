9720489d9e54cc47348aade8bd833441b0f59f7b
cadaverine <kennville@gmail.com>
2019-04-21 23:53:30 +0300

    feat(main.go): updated

--- main.go
+++ main.go
@@ -7,28 +7,31 @@ import (
 )
 
 func main() {
-	gr := graph.InitializeGraph()
-
-	gr.AddVertex(0, 12)
-	gr.AddVertex(1, 32)
-	gr.AddVertex(2, 54)
-	gr.AddVertex(3, 77)
-	gr.AddVertex(4, 34)
-	gr.AddVertex(5, 73)
-	gr.AddVertex(6, 57)
-	gr.AddVertex(7, 76)
-
-	gr.AddEdgeByIDs(0, 2, 5)
-	gr.AddEdgeByIDs(0, 3, 2)
-	gr.AddEdgeByIDs(4, 6, 1)
-	gr.AddEdgeByIDs(2, 0, 3)
-	gr.AddEdgeByIDs(0, 1, 1)
-	gr.AddEdgeByIDs(1, 6, 4)
-	gr.AddEdgeByIDs(6, 3, 3)
-	gr.AddEdgeByIDs(6, 5, 1)
-	gr.AddEdgeByIDs(7, 3, 2)
-	gr.AddEdgeByIDs(7, 5, 3)
-	gr.AddEdgeByIDs(5, 7, 2)
+	gr := graph.CreateFromString(`
+		{
+			(0, 12),
+			(1, 32),
+			(2, 54),
+			(3, 77),
+			(4, 34),
+			(5, 73),
+			(6, 57),
+			(7, 76)
+		},
+		{
+			(0, 2, 5),
+			(0, 3, 2),
+			(4, 6, 1),
+			(2, 0, 3),
+			(0, 1, 1),
+			(1, 6, 4),
+			(6, 3, 3),
+			(6, 5, 1),
+			(7, 3, 2),
+			(7, 5, 3),
+			(5, 7, 2)
+		}
+	`)
 
 	fmt.Println(gr)
 
